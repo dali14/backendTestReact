6c7d3ed7108587115731b7270d334bb1abacb5ad
dali14 <dalijlasii@gmail.com>
2023-05-14 10:23:09 +0100

    db config

---
+++ db.js
@@ -0,0 +1,19 @@
+const { Sequelize } = require('sequelize');
+
+// Create a new Sequelize instance
+const sequelize = new Sequelize('User', 'root', 'dali14', {
+  host: 'localhost',
+  dialect: 'mysql'
+});
+
+// Test the database connection
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Database connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+})();
+
+module.exports = sequelize;
