1deccf1840b187f20d9196ae758d3a57d2e96d3a
dali14 <dalijlasii@gmail.com>
2023-05-14 10:22:45 +0100

    user controller

---
+++ controllers/userControllers.js
@@ -0,0 +1,35 @@
+const User = require('../models/User');
+
+const allUser = (req, res) => {
+    User.findAll()
+      .then(users => res.json(users))
+      .catch(err => res.status(500).json({ error: err.message }));
+  }
+const getUsersByIds = (req, res) => {
+    User.findAll({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(users => res.json(users))
+    .catch(err => res.status(500).json({ error: err.message }));
+}
+
+
+const createUser = (req, res) => {
+    User.create(req.body)
+      .then(user => res.json(user))
+      .catch(err => res.status(500).json({ error: err.message }));
+  }
+
+const updateUser = (req, res) => {
+    User.update(req.body, {
+    where: { id: req.params.id }
+    })
+    .then(user => res.json(user))
+    .catch(err => res.status(500).json({ error: err.message }));
+}
+  module.exports = {
+    allUser,
+    createUser  
+    }
